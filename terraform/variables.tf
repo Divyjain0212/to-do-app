4c1dc2f60a1901681b7d99c28f6fc63bc8e3bad0
Divyjain0212 <divyj12349@gmail.com>
2026-03-29 07:42:31 +0000

    Fixed rolling updates

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -7,7 +7,7 @@ variable "aws_region" {
 variable "aws_profile" {
   description = "AWS shared config profile used by Terraform"
   type        = string
-  default     = "default"
+  default     = ""
 }
 
 variable "project_name" {
