1350e71e7a0663b4fe7f507c99a108562330e340
Divyjain0212 <divyj12349@gmail.com>
2026-04-02 00:27:47 +0000

    fixed user-data.sh

--- terraform/modules/compute/user-data.sh
+++ terraform/modules/compute/user-data.sh
@@ -14,9 +14,9 @@ chmod a+r /etc/apt/keyrings/docker.asc
 tee /etc/apt/sources.list.d/docker.sources > /dev/null <<EOF
 Types: deb
 URIs: https://download.docker.com/linux/ubuntu
-Suites: \$(. /etc/os-release && echo "\${UBUNTU_CODENAME:-\$VERSION_CODENAME}")
+Suites: $$(. /etc/os-release && echo "$${UBUNTU_CODENAME:-$${VERSION_CODENAME}}")
 Components: stable
-Architectures: \$(dpkg --print-architecture)
+Architectures: $$(dpkg --print-architecture)
 Signed-By: /etc/apt/keyrings/docker.asc
 EOF
 
